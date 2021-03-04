de2bb14a1636a1c291e352067a086d9816de18f6
Steve Yalcin <steve.yalcin@ibm.com>
2021-03-04 13:47:12 -0500

    Axios is removed from package json

--- src/components/Kanata.js
+++ src/components/Kanata.js
@@ -1,27 +1,36 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 export default function Kanata() {
   const [data, setData] = useState([]);
 
+  async function callKanataApi() {
+    await fetch(
+      "https://us-central1-kanata-production.cloudfunctions.net/api/content"
+    )
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .then((result) => {
+        console.log("Success:", result);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
+
   useEffect(() => {
-    axios
-      .get(
-        'https://us-central1-kanata-production.cloudfunctions.net/api/content'
-      )
-      .then(result => setData(result.data));
+    callKanataApi();
   }, []);
 
   return (
     <div>
-      {data.map(item => (
+      {data.map((item) => (
         <img
           key={item.contentId}
           src={item.image}
           alt=""
           height="250px"
           width="250px"
-          style={{ margin: '1em' }}
+          style={{ margin: "1em" }}
         />
       ))}
     </div>
